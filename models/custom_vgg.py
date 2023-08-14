af07d10babb9afb70e42a4b18d72b987cadf5b92
renanj <jacomassi.r@gmail.com>
2023-08-14 02:28:05 -0300

    custo mvgg

--- models/custom_vgg.py
+++ models/custom_vgg.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import torchvision.models as models
 
 class CustomVGG(nn.Module):
-    def __init__(self, architecture='vgg11', freeze_method=None):
+    def __init__(self, num_classes=10, channels=3, architecture='vgg11', freeze_method=None):
         super(CustomVGG, self).__init__()
 
         if architecture == 'vgg11':
@@ -13,19 +13,24 @@ class CustomVGG(nn.Module):
         else:
             raise ValueError("Unknown architecture")
 
+        # Adjust for different number of input channels
+        if channels != 3:
+            self.model.features[0] = nn.Conv2d(channels, 64, kernel_size=3, stride=1, padding=1)
+
         self.adjust_parameters(freeze_method)
+
+        # Modify the final layer
+        self.model.classifier[-1] = nn.Linear(self.model.classifier[-1].in_features, num_classes)
 
     def adjust_parameters(self, freeze_method):
         if freeze_method == 'from_scratch':
             self.model = self.model.apply(self._reset_weights)
         elif freeze_method == 'pre_trained_unfreeze_top_layer':
-            for param in self.model.parameters():
-                param.requires_grad = False
+            self._freeze_all()
             for param in self.model.classifier[-1].parameters():
                 param.requires_grad = True
         elif freeze_method == 'pre_trained_unfreeze_partial_last_layers':
-            for param in self.model.parameters():
-                param.requires_grad = False
+            self._freeze_all()
             for param in self.model.classifier[-3:].parameters():
                 param.requires_grad = True
         elif freeze_method == 'pre_trained_unfreeze_all_layers':
@@ -36,6 +41,10 @@ class CustomVGG(nn.Module):
     def _reset_weights(self, m):
         if isinstance(m, (nn.Conv2d, nn.Linear)):
             m.reset_parameters()
+
+    def _freeze_all(self):
+        for param in self.model.parameters():
+            param.requires_grad = False
 
     def forward(self, x):
         return self.model(x)
