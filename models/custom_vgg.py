0f1a9c21152bb70815bc0eccb66bb64afd91ea8d
renanj <jacomassi.r@gmail.com>
2023-08-15 03:20:33 -0300

    vgg

--- models/custom_vgg.py
+++ models/custom_vgg.py
@@ -9,34 +9,34 @@ class CustomVGG(nn.Module):
         self.embDim = 512
 
         if architecture == 'vgg11':
-            base_model = models.vgg11(pretrained=True)
+            self.base_model = models.vgg11(pretrained=True)
         elif architecture == 'vgg16':
-            base_model = models.vgg16(pretrained=True)
+            self.base_model = models.vgg16(pretrained=True)
         else:
             raise ValueError("Unknown architecture")
 
         # Adjust for different number of input channels
         if channels != 3:
-            base_model.features[0] = nn.Conv2d(channels, 64, kernel_size=3, stride=1, padding=1)
+            self.base_model.features[0] = nn.Conv2d(channels, 64, kernel_size=3, stride=1, padding=1)
 
-        self.features = base_model.features  # Expose features directly
-
-        self.adjust_parameters(freeze_method, base_model)
+        self.adjust_parameters(freeze_method)
 
         # Modify the final layer
-        base_model.classifier[-1] = nn.Linear(base_model.classifier[-1].in_features, num_classes)
-        self.classifier = base_model.classifier  # Expose classifier directly
+        self.base_model.classifier[-1] = nn.Linear(self.base_model.classifier[-1].in_features, num_classes)
+        
+        # Add a dummy attribute for compatibility with batch_bald.py
+        self.linear = self.base_model.classifier
 
-    def adjust_parameters(self, freeze_method, model):
+    def adjust_parameters(self, freeze_method):
         if freeze_method == 'from_scratch':
-            model = model.apply(self._reset_weights)
+            self.base_model = self.base_model.apply(self._reset_weights)
         elif freeze_method == 'pre_trained_unfreeze_top_layer':
-            self._freeze_all(model)
-            for param in model.classifier[-1].parameters():
+            self._freeze_all()
+            for param in self.base_model.classifier[-1].parameters():
                 param.requires_grad = True
         elif freeze_method == 'pre_trained_unfreeze_partial_last_layers':
-            self._freeze_all(model)
-            for param in model.classifier[-3:].parameters():
+            self._freeze_all()
+            for param in self.base_model.classifier[-3:].parameters():
                 param.requires_grad = True
         elif freeze_method == 'pre_trained_unfreeze_all_layers':
             pass
@@ -47,19 +47,19 @@ class CustomVGG(nn.Module):
         if isinstance(m, (nn.Conv2d, nn.Linear)):
             m.reset_parameters()
 
-    def _freeze_all(self, model):
-        for param in model.parameters():
+    def _freeze_all(self):
+        for param in self.base_model.parameters():
             param.requires_grad = False
 
     def forward(self, x, last=False, freeze=False):
         if freeze:
             with torch.no_grad():
-                out = self.features(x)
+                out = self.base_model.features(x)
                 e = out.view(out.size(0), -1)
         else:
-            out = self.features(x)
+            out = self.base_model.features(x)
             e = out.view(out.size(0), -1)
-        out = self.classifier(e)
+        out = self.base_model.classifier(e)
         if last:
             return out, e
         else:
